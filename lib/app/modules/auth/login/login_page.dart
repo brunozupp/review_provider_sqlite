a5b98635feb28018d6248bfdcca0f1d6c9c3fcdb
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-06-17 16:36:30 -0300

    Built component: TodoListLogo

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:review_provider_sqlite/app/core/ui/components/todo_list_logo.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,10 +7,29 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+                minWidth: constraints.maxWidth,
+              ),
+              child: IntrinsicHeight( // Using this the Column will have the exact size of its children's size
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const TodoListLogo(),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
-      body: Container(),
     );
   }
 }
